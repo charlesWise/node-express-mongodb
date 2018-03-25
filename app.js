c6d100c9c687fd6ec5511136f68d27141225c642
chenrunsheng1991 <742068070@qq.com>
2018-03-25 14:54:51 +0800

    '静态文件托管'

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var swig = require('swig'); //加载模板引擎
 
 var app = express();
 
+//设置静态文件托管
+//当用户访问的url以/public开始，那么直接返回对应__dirname + '/public'下的文件
+app.use('/public', express.static(__dirname + '/public'));
+
 //配置应用模块，定义当前应用所使用的模板引擎，第一个参数：模板引擎名称同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
 app.engine('html', swig.renderFile);
 //设置模板文件存放的目录，第一个参数是必须是views，第二个参数的目录
