427118abaa9fe8f23a6138c591bcbcfcf806fc87
lianqin <chenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.com>
2018-04-01 10:15:45 +0800

    user model

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', './views');
 //注册所使用的模板引擎，第一个参数必须是view engine，第二个参数是和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
 app.set('view engine', 'html');
 //在开发过程中，需要取消模板缓存
-swig.setDefaults({cache: false});
+swig.setDefaults({ cache: false });
 
 /**
  * 根据不同的功能划分模块
@@ -28,15 +28,21 @@ app.use('/api', require('./routers/api'));
 app.use('/', require('./routers/main'));
 
 // app.get('/', function(req, res, next) {
-    // res.send('<h1>欢迎来到我的地盘</h1>');
+// res.send('<h1>欢迎来到我的地盘</h1>');
 
-    /**
-     * 读取views目录下的指定文件，解析并返回给客户端
-     * 第一个参数，表示模板的文件，相对于views目录 view/index.html
-     * 第二个参数，传递给模板使用的数据
-     */
+/**
+ * 读取views目录下的指定文件，解析并返回给客户端
+ * 第一个参数，表示模板的文件，相对于views目录 view/index.html
+ * 第二个参数，传递给模板使用的数据
+ */
 //     res.render('index');
 // })
 
-mongoose.connect();
-app.listen(8080);
+mongoose.connect('mongodb://localhost:27018/bolg', function(err) {
+	if(err) {
+		console.log('数据库连接失败！');
+	}else {
+		console.log('数据库连接成功！');
+		app.listen(8080);
+	}
+});
--- schemas/users.js
+++ schemas/users.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose');
+
+// 用户对表结构
+module.exports = new mongoose.Schema({
+	// 用户名
+	username: String,
+	// 密码
+	password: String
+})
